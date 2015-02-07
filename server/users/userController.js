1d480335b866d09a6e679b249e47d5998561752c
Keenan Lidral-Porter <klp2012@gmail.com>
2015-02-07 14:36:33 -0800

    (doc) fixes numerous spelling/grammar errors

--- server/users/userController.js
+++ server/users/userController.js
@@ -64,8 +64,8 @@ module.exports = {
 
   checkAuth: function (req, res, next) {
     // checking to see if the user is authenticated
-    // grab the token in the header is any
-    // then decode the token, which we end up being the user object
+    // grab the token in the header if any
+    // then decode the token, which will end up being the user object
     // check to see if that user exists in the database
     var token = req.headers['x-access-token'];
     if (!token) {
